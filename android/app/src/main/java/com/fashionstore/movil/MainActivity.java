de3b621dba240cbd575661202b65e7377e0a32ef
Cristian <cmamani096@gmail.com>
2026-09-22 02:52:27 -0400

    fix: update android build configuration, pubspec locks and test suite for mobile release

---
+++ android/app/src/main/java/com/fashionstore/movil/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fashionstore.movil;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/app/src/main/kotlin/com/example/fashionstore_movil/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fashionstore_movil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
